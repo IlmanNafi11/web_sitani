fdc8a751e9a87c85a065c52a6c03de9c8e0af506
IlmanNafi11 <studyilmannafi@gmail.com>
2025-02-28 22:15:04 +0700

    mutasi data sebelum disimpan dengan style title case dan mengatur title halaman create

--- app/Filament/Resources/KecamatanResource/Pages/CreateKecamatan.php
+++ app/Filament/Resources/KecamatanResource/Pages/CreateKecamatan.php
@@ -2,13 +2,22 @@
 
 namespace App\Filament\Resources\KecamatanResource\Pages;
 
-use App\Filament\Resources\KecamatanResource;
-use Filament\Actions;
+use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\KecamatanResource;
 
 class CreateKecamatan extends CreateRecord
 {
     protected static string $resource = KecamatanResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nama'] = Str::title($data['nama']);
+
+        return $data;
+    }
+
+    protected static ?string $title = "Tambah Kecamatan";
 
     protected function getRedirectUrl(): string
     {
