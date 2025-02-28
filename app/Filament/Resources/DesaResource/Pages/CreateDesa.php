8e5190087488a0064bbae70006c8dc1c63a6ea80
IlmanNafi11 <studyilmannafi@gmail.com>
2025-02-28 22:37:48 +0700

    mutasi data sebelum disimpan dengan style title case dan mengatur redirect setelah proses create record

--- app/Filament/Resources/DesaResource/Pages/CreateDesa.php
+++ app/Filament/Resources/DesaResource/Pages/CreateDesa.php
@@ -2,11 +2,24 @@
 
 namespace App\Filament\Resources\DesaResource\Pages;
 
-use App\Filament\Resources\DesaResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
+use App\Filament\Resources\DesaResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDesa extends CreateRecord
 {
     protected static string $resource = DesaResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nama'] = Str::title($data['nama']);
+
+        return $data;
+    }
 }
