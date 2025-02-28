35b6a236cc978ebbba50b0513c4a41e66d0733e9
IlmanNafi11 <studyilmannafi@gmail.com>
2025-02-28 21:48:40 +0700

    mengatur redirect halaman setelah proses create record

--- app/Filament/Resources/KecamatanResource/Pages/CreateKecamatan.php
+++ app/Filament/Resources/KecamatanResource/Pages/CreateKecamatan.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateKecamatan extends CreateRecord
 {
     protected static string $resource = KecamatanResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
