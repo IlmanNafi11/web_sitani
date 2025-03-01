ae845981378fb7d58ae11651e034e140a20accdd
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-01 10:23:05 +0700

    menambahkan attribute no hp, alamat, path profile, dan role kedalam fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'no_hp',
+        'alamat',
+        'path_profile',
+        'role',
     ];
 
     /**
