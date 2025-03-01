fa8c216880f01fc16344b0a91cd93ddd1c51a337
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-01 09:57:42 +0700

    mutasi data sebelum disimpan dengan style title case dan mengatur redirect setelah proses create record

--- app/Filament/Resources/KelompokTaniResource/Pages/CreateKelompokTani.php
+++ app/Filament/Resources/KelompokTaniResource/Pages/CreateKelompokTani.php
@@ -2,11 +2,24 @@
 
 namespace App\Filament\Resources\KelompokTaniResource\Pages;
 
-use App\Filament\Resources\KelompokTaniResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\KelompokTaniResource;
 
 class CreateKelompokTani extends CreateRecord
 {
     protected static string $resource = KelompokTaniResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nama'] = Str::title($data['nama']);
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
