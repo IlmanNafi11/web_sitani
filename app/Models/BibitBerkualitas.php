c32171096accfcb433b95d19ec87c93fdddaf667
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-01 11:09:50 +0700

    menambahkan attibute yang dapat di mass assigment kedalam fillable

--- app/Models/BibitBerkualitas.php
+++ app/Models/BibitBerkualitas.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BibitBerkualitas extends Model
 {
-    //
+    /**
+     * Tambahkan attribute yang dapat dimass assigment kedalam array fillable
+     * @var array
+     */
+    protected $fillable = [
+        'nama',
+        'komoditas_id',
+        'deskripsi',
+    ];
 }
--- app/Models/Komoditas.php
+++ app/Models/Komoditas.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Komoditas extends Model
 {
-    //
+    /**
+     * Tambahkan attribute yang dapat dimass assigment kedalam array fillable
+     * @var array
+     */
+    protected $fillable = [
+        'nama',
+        'deskripsi',
+    ];
 }
