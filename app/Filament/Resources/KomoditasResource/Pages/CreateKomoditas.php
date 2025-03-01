d6b3a9ec8aeba8597ebb190a80657b786d4da01f
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-01 11:33:23 +0700

    mutasi data sebelum disimpan dengan style title case dan mengatur redirect halaman setelah proses create record

--- app/Filament/Resources/KomoditasResource/Pages/CreateKomoditas.php
+++ app/Filament/Resources/KomoditasResource/Pages/CreateKomoditas.php
@@ -2,11 +2,24 @@
 
 namespace App\Filament\Resources\KomoditasResource\Pages;
 
-use App\Filament\Resources\KomoditasResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\KomoditasResource;
 
 class CreateKomoditas extends CreateRecord
 {
     protected static string $resource = KomoditasResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nama'] = Str::title($data['nama']);
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
