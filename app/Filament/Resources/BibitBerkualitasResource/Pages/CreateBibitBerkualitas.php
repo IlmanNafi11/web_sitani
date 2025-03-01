dfaa0baa845d1d0339736bcd241d029fee0c247c
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-01 11:47:15 +0700

    mutasi data sebelum disimpan dengan style title case dan mengatur redirect setelah create record

--- app/Filament/Resources/BibitBerkualitasResource/Pages/CreateBibitBerkualitas.php
+++ app/Filament/Resources/BibitBerkualitasResource/Pages/CreateBibitBerkualitas.php
@@ -2,11 +2,24 @@
 
 namespace App\Filament\Resources\BibitBerkualitasResource\Pages;
 
-use App\Filament\Resources\BibitBerkualitasResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\BibitBerkualitasResource;
 
 class CreateBibitBerkualitas extends CreateRecord
 {
     protected static string $resource = BibitBerkualitasResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nama'] = Str::title($data['nama']);
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
