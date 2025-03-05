a8a09e55498a15055ca01b451c9b8b79dfaf0026
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-05 13:36:16 +0700

    mutasi data nama sebelum disimpan menggunakan style title case

--- app/Filament/Resources/PenyuluhTerdaftarResource/Pages/CreatePenyuluhTerdaftar.php
+++ app/Filament/Resources/PenyuluhTerdaftarResource/Pages/CreatePenyuluhTerdaftar.php
@@ -2,11 +2,19 @@
 
 namespace App\Filament\Resources\PenyuluhTerdaftarResource\Pages;
 
-use App\Filament\Resources\PenyuluhTerdaftarResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\PenyuluhTerdaftarResource;
 
 class CreatePenyuluhTerdaftar extends CreateRecord
 {
     protected static string $resource = PenyuluhTerdaftarResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['nama'] = Str::title($data['nama']);
+
+        return $data;
+    }
 }
