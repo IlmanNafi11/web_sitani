1dad3feff843b2f6e6e69d6dede5f962609ea60c
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-05 13:37:34 +0700

    mengatur redirect setelah create record ke route index(halaman utama)

--- app/Filament/Resources/PenyuluhTerdaftarResource/Pages/CreatePenyuluhTerdaftar.php
+++ app/Filament/Resources/PenyuluhTerdaftarResource/Pages/CreatePenyuluhTerdaftar.php
@@ -17,4 +17,9 @@ class CreatePenyuluhTerdaftar extends CreateRecord
 
         return $data;
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
