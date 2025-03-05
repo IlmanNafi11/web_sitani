f8e739c3eebdb24b661079af48a55eee3ab62408
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-05 13:28:48 +0700

    mengatur resource penyuluh terdaftar (form dan table)

--- app/Filament/Resources/PenyuluhTerdaftarResource.php
+++ app/Filament/Resources/PenyuluhTerdaftarResource.php
@@ -6,9 +6,13 @@ use App\Filament\Resources\PenyuluhTerdaftarResource\Pages;
 use App\Filament\Resources\PenyuluhTerdaftarResource\RelationManagers;
 use App\Models\PenyuluhTerdaftar;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +27,21 @@ class PenyuluhTerdaftarResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('nama')
+                ->label('Nama')
+                ->placeholder('Nama penyuluh'),
+                TextInput::make('no_hp')
+                ->label('No Hp')
+                ->placeholder('No hp penyuluh'),
+                TextInput::make('alamat')
+                ->label('Alamat')
+                ->placeholder('Alamat penyuluh'),
+                Select::make('kecamatan_id')
+                ->label('Kecamatan')
+                ->placeholder('Kecamatan')
+                ->relationship('kecamatan', 'nama')
+                ->searchable()
+                ->preload(),
             ]);
     }
 
@@ -31,13 +49,25 @@ class PenyuluhTerdaftarResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('nama')
+                ->label('Nama')
+                ->searchable(),
+                TextColumn::make('no_hp')
+                ->label('No Hp')
+                ->searchable(),
+                TextColumn::make('alamat')
+                ->label('Alamat'),
+                TextColumn::make('kecamatan.nama')
+                ->label('Kecamatan'),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make()
+                ->button(),
+                DeleteAction::make()
+                ->button(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
