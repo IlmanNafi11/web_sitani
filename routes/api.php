01c819eb3cb17d6afea8f50e9db5db34c1b83481
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-24 05:56:26 +0700

    mengeluarkan endpoint login dan validate-phone dari middleware api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\Authentication\AuthController;
+use App\Http\Controllers\Authentication\AuthPhoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [AuthController::class, 'login'])->withoutMiddleware('api');
+Route::post('validate-phone', [AuthPhoneController::class, 'checkPhone']);
 
 Route::group([
     "middleware" => "api",
 ], function () {
-    Route::post('login', [AuthController::class, 'login'])->withoutMiddleware('api');
+
 });
+
