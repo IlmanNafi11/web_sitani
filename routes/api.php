990cb4bc7f24dc32be062dbde506d3b4cbd3bd30
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-25 16:36:25 +0700

    membuat endpoint untuk mengambil semua data komoditas dan berdasarkan id

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Authentication\AuthController;
 use App\Http\Controllers\Authentication\AuthPhoneController;
+use App\Http\Controllers\KomoditasController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,5 +13,7 @@ Route::group([
     "middleware" => "auth:api",
 ], function () {
     Route::get('/user/{id}', [UserController::class, 'getUserById'])->whereNumber('id');
+    Route::get('/komoditas', [KomoditasController::class, 'getAll']);
+    Route::get('/komoditas/{id}', [KomoditasController::class, 'getById'])->whereNumber('id');
 });
 
