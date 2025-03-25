9c3615cebd7f0a1c45884bb422d45311f96ad37d
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-25 17:48:42 +0700

    membuat interface Kelompok tani repository

---
+++ app/Repositories/Interfaces/KelompokTaniRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Models\KelompokTani;
+use Illuminate\Database\Eloquent\Collection;
+
+interface KelompokTaniRepositoryInterface
+{
+    /**
+     * Mengambil seluruh data kelompok tani berdasarkan penyuluh terdaftar id
+     * @param int $id ID penyuluh terdaftar
+     * @return Collection
+     */
+    public function getAllByPenyuluh($id): Collection;
+
+    /**
+     * Mengambil data kelompok tani berdasarkan id
+     * @param int $id ID kelompok tani
+     * @return KelompokTani
+     */
+    public function getById($id): KelompokTani;
+}
