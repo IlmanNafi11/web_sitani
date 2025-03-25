1ad25dc768a0de855e09ac6586ac8b1f014f00a3
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-25 21:31:08 +0700

    membuat bibit berkualitas service

---
+++ app/Services/BibitBerkualitasService.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\Interfaces\BibitRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+
+class BibitBerkualitasService{
+    protected BibitRepositoryInterface $bibitRepository;
+
+    public function __construct(BibitRepositoryInterface $bibitRepository)
+    {
+        $this->bibitRepository = $bibitRepository;
+    }
+
+    /**
+     * Mengambil seluruh data bibit berkulitas
+     * @return JsonResponse
+     */
+    public function getAll(): JsonResponse
+    {
+        $data = $this->bibitRepository->getAll();
+        if ($data->isEmpty()) {
+            return response()->json([
+                'message' => 'Bibit Berkualitas is Empty'
+            ], 404);
+        }
+
+        return response()->json($data);
+    }
+}
