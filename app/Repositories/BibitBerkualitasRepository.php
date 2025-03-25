7514e06b5746e30388ed1226f4690cb39223812f
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-25 21:33:53 +0700

    generate namespace dan memperbaiki return type

--- app/Repositories/BibitBerkualitasRepository.php
+++ app/Repositories/BibitBerkualitasRepository.php
@@ -1,11 +1,13 @@
 <?php
+namespace App\Repositories;
 
 use App\Models\BibitBerkualitas;
 use App\Repositories\Interfaces\BibitRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class BibitBerkualitasRepository implements BibitRepositoryInterface{
 
-    public function getAll(): Illuminate\Database\Eloquent\Collection|null
+    public function getAll(): Collection
     {
         return BibitBerkualitas::all();
     }
