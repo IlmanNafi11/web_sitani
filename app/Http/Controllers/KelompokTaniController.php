8dbb5a828846193190d70a9a777b63ba245ba319
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-25 20:39:32 +0700

    membuat metode getAllByPenyuluh untuk mengambil data berdasarkan penyuluh

--- app/Http/Controllers/KelompokTaniController.php
+++ app/Http/Controllers/KelompokTaniController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\KelompokTaniService;
 
 class KelompokTaniController extends Controller
 {
-    
+    protected KelompokTaniService $kelompokTaniService;
+
+    public function __construct(KelompokTaniService $kelompokTaniService)
+    {
+        $this->kelompokTaniService = $kelompokTaniService;
+    }
+    public function getAllByPenyuluh(Request $request)
+    {
+        $id = $request->query('id');
+        return $this->kelompokTaniService->getAllByPenyuluh($id);
+    }
 }
