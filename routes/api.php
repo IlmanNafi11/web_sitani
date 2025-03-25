f7b3ccac549e862f4150fbadabf643240e5c139b
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-25 15:34:00 +0700

    menambahkan endpoint untuk get data user berdasarkan id

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use App\Http\Controllers\Authentication\AuthController;
 use App\Http\Controllers\Authentication\AuthPhoneController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])->withoutMiddleware('api');
 Route::post('validate-phone', [AuthPhoneController::class, 'checkPhone']);
-
 Route::group([
-    "middleware" => "api",
+    "middleware" => "auth:api",
 ], function () {
-
+    Route::get('/user/{id}', [UserController::class, 'getUserById'])->whereNumber('id');
 });
 
