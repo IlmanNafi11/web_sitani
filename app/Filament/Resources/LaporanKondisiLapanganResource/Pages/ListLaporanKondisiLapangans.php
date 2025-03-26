f1eb713345c4dbcbbc1d24634031425c7875d93c
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-26 17:15:30 +0700

    hapus create resource button

--- app/Filament/Resources/LaporanKondisiLapanganResource/Pages/ListLaporanKondisiLapangans.php
+++ app/Filament/Resources/LaporanKondisiLapanganResource/Pages/ListLaporanKondisiLapangans.php
@@ -13,7 +13,7 @@ class ListLaporanKondisiLapangans extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //
         ];
     }
 }
