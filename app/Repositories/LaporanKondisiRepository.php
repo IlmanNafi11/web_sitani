14f9939c8c6643fd9eb1ec47a91347b417712d32
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-26 12:56:10 +0700

    memperbarui dengan attribute yang spesifik

--- app/Repositories/LaporanKondisiRepository.php
+++ app/Repositories/LaporanKondisiRepository.php
@@ -2,11 +2,16 @@
 namespace App\Repositories;
 
 use App\Models\LaporanKondisiLapangan;
+use App\Models\LaporanKondisiLapanganDetail;
 use App\Repositories\Interfaces\LaporanInterface;
  class LaporanKondisiRepository implements LaporanInterface{
 
     public function saveReport(array $data): LaporanKondisiLapangan
     {
-        return LaporanKondisiLapangan::create($data);
+        return LaporanKondisiLapangan::create([
+            'kelompok_tani_id' => $data['kelompok_tani_id'],
+            'komoditas_id' => $data['komoditas_id'],
+            'penyuluh_id' => $data['penyuluh_id'],
+        ]);
     }
  }
