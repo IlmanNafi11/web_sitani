63061651d0ef2780d895529a1a7e4792b934e86c
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-26 14:24:03 +0700

    membuat view laporan kondisi lapangan

---
+++ app/Filament/Resources/LaporanKondisiLapanganResource/Pages/ViewLaporanKondisiLapangan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\LaporanKondisiLapanganResource\Pages;
+
+use App\Filament\Resources\LaporanKondisiLapanganResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewLaporanKondisiLapangan extends ViewRecord
+{
+    protected static string $resource = LaporanKondisiLapanganResource::class;
+
+}
