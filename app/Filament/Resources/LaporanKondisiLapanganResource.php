2a8a89d6ece49d0c688d290500877932b6c71d98
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-26 17:34:02 +0700

    menambahkan action, dan info list untuk view detail laporan

--- app/Filament/Resources/LaporanKondisiLapanganResource.php
+++ app/Filament/Resources/LaporanKondisiLapanganResource.php
@@ -2,16 +2,28 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\LaporanKondisiLapanganResource\Pages;
-use App\Filament\Resources\LaporanKondisiLapanganResource\RelationManagers;
-use App\Models\LaporanKondisiLapangan;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+use Filament\Pages\Page;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use App\Models\LaporanKondisiLapangan;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\LaporanKondisiLapanganResource\Pages;
+use App\Filament\Resources\LaporanKondisiLapanganResource\RelationManagers;
+use Filament\Infolists\Components\Group;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Tabs;
+use Filament\Infolists\Components\Tabs\Tab;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ViewAction;
 
 class LaporanKondisiLapanganResource extends Resource
 {
@@ -31,13 +43,70 @@ class LaporanKondisiLapanganResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('kelompokTani.nama')
+                    ->label('Kelompok Tani')
+                    ->searchable(),
+                TextColumn::make('penyuluh.penyuluhTerdaftar.nama')
+                    ->label('Pelapor')
+                    ->searchable(),
+                TextColumn::make('laporanKondisiLapanganDetail.luas_lahan')
+                    ->label('Luas Lahan/Ha')
+                    ->sortable(),
+                TextColumn::make('laporanKondisiLapanganDetail.jenis_bibit')
+                    ->label('Bibit')
+                    ->searchable(),
+                TextColumn::make('laporanKondisiLapanganDetail.estimasi_panen')
+                    ->label('Estimasi Panen')
+                    ->sortable(),
+                TextColumn::make('status')
+                    ->label('Status')
+                    ->searchable()
+                    ->badge()
+                    ->color(function ($record) {
+                        switch ($record->status) {
+                            case 'tidak berkualitas':
+                                return 'danger';
+                            case 'berkualitas':
+                                return 'success';
+                            default:
+                                return 'warning';
+                        }
+                    })
+                    ->icon(fn($record) => match ($record->status) {
+                        'tidak berkualitas' => 'iconpark-badtwo-o',
+                        'berkualitas' => 'iconpark-goodtwo-o',
+                        default => 'heroicon-o-clock',
+                    }),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    Action::make('berkualitas')
+                        ->label('Berkualitas')
+                        ->color('success')
+                        ->action(function (LaporanKondisiLapangan $record) {
+                            $record->update(['status' => 'berkualitas']);
+                        })
+                        ->icon('iconpark-goodtwo-o'),
+                    Action::make('tidak_berkualitas')
+                        ->label('Tidak Berkualitas')
+                        ->color('danger')
+                        ->action(function (LaporanKondisiLapangan $record) {
+                            $record->update(['status' => 'tidak berkualitas']);
+                        })
+                        ->icon('iconpark-badtwo-o'),
+                ])
+                    ->button()
+                    ->label('Perbarui Status'),
+                ActionGroup::make([
+                    ViewAction::make('view')
+                        ->label('Lihat'),
+                    DeleteAction::make()
+                        ->label('Hapus'),
+                ])
+                    ->button(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -57,8 +126,81 @@ class LaporanKondisiLapanganResource extends Resource
     {
         return [
             'index' => Pages\ListLaporanKondisiLapangans::route('/'),
-            'create' => Pages\CreateLaporanKondisiLapangan::route('/create'),
-            'edit' => Pages\EditLaporanKondisiLapangan::route('/{record}/edit'),
         ];
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Tabs::make('Tabs')
+                    ->tabs([
+                        Tab::make('Laporan Detail')
+                            ->schema([
+                                Group::make([
+                                    TextEntry::make('kelompokTani.nama')
+                                        ->label('Kelompok Tani'),
+                                    TextEntry::make('penyuluh.penyuluhTerdaftar.nama')
+                                        ->label('Pelapor'),
+                                    TextEntry::make('laporanKondisiLapanganDetail.luas_lahan')
+                                        ->label('Luas Lahan/Ha'),
+                                    TextEntry::make('status')
+                                        ->label('Status')
+                                        ->badge()
+                                        ->color(function ($record) {
+                                            switch ($record->status) {
+                                                case 'tidak berkualitas':
+                                                    return 'danger';
+                                                case 'berkualitas':
+                                                    return 'success';
+                                                default:
+                                                    return 'warning';
+                                            }
+                                        })
+                                        ->icon(fn($record) => match ($record->status) {
+                                            'tidak berkualitas' => 'iconpark-badtwo-o',
+                                            'berkualitas' => 'iconpark-goodtwo-o',
+                                            default => 'heroicon-o-clock',
+                                        }),
+                                ]),
+                                Group::make([
+                                    TextEntry::make('laporanKondisiLapanganDetail.jenis_bibit')
+                                        ->label('Bibit'),
+                                    TextEntry::make('laporanKondisiLapanganDetail.estimasi_panen')
+                                        ->label('Estimasi Panen'),
+                                    ImageEntry::make('laporanKondisiLapanganDetail.foto_bibit')
+                                        ->label('Foto Bibit'),
+                                ]),
+                            ])
+                            ->columns(2)
+                            ->icon('css-details-more'),
+                        Tab::make('Detail Pelapor')
+                            ->schema([
+                                Group::make([
+                                    TextEntry::make('penyuluh.penyuluhTerdaftar.nama')
+                                        ->label('Nama'),
+                                    TextEntry::make('penyuluh.penyuluhTerdaftar.no_hp')
+                                        ->label('Telepon')
+                                        ->copyable(),
+                                    TextEntry::make('penyuluh.penyuluhTerdaftar.alamat')
+                                        ->label('Alamat'),
+                                    TextEntry::make('penyuluh.user.email')
+                                        ->label('Email')
+                                        ->copyable(),
+                                ]),
+                                Group::make([
+                                    TextEntry::make('penyuluh.user.role')
+                                        ->label('Role'),
+                                    TextEntry::make('kelompokTani.desa.nama')
+                                        ->label('Desa'),
+                                    TextEntry::make('kelompokTani.kecamatan.nama')
+                                        ->label('Kecamatan'),
+                                ]),
+                            ])
+                            ->columns(2)
+                            ->icon('phosphor-user-list'),
+                    ])
+                    ->columnSpanFull(),
+            ]);
     }
 }
