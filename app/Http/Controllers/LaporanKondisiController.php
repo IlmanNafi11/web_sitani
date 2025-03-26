62a1dd604976b8a035be6264760ea94909ec387b
IlmanNafi11 <studyilmannafi@gmail.com>
2025-03-26 12:58:22 +0700

    menambahkan metode sendReport untuk menerima laporan kondisi dari client

--- app/Http/Controllers/LaporanKondisiController.php
+++ app/Http/Controllers/LaporanKondisiController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LaporanKondisiRequest;
+use App\Services\LaporanKondisiService;
 use Illuminate\Http\Request;
 
 class LaporanKondisiController extends Controller
 {
-    //
+    protected LaporanKondisiService $laporanKondisiService;
+
+    public function __construct(LaporanKondisiService $laporanKondisiService)
+    {
+        $this->laporanKondisiService = $laporanKondisiService;
+    }
+    public function sendReport(LaporanKondisiRequest $request)
+    {
+        $validated = $request->validated();
+
+        $laporanKondisi = $this->laporanKondisiService->saveLaporan($validated);
+
+        if ($laporanKondisi) {
+            return response()->json([
+                'message' => 'Report sent successfully',
+                'data' => $laporanKondisi,
+            ], 201);
+        }
+    }
 }
